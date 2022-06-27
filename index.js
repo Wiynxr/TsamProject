f3292df29da9e3f1ef5fb0cd6b2d40b4d8f24571
TAPAEB <aleksejpushkin223@gmail.com>
2022-06-27 11:02:32 +0300

    Турпал сделал это задание

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ const mongoose = require("mongoose");
 
 const app = express();
 
-app.use(express.json());
-
 mongoose
   .connect(
     "mongodb+srv://fringle:12345@cluster0.wgisrd8.mongodb.net/tsamProject",
@@ -16,6 +14,6 @@ mongoose
   .then(() => console.log("Успешно соединились с сервером MongoDB"))
   .catch(() => console.log("Ошибка при соединении с сервером MongoDB"));
 
-app.listen(2999, () => {
+app.listen(2999), () => {
   console.log(`Для подключения перейдите на хост http://localhost:${2999}`);
-});
+};
